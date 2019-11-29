28b8a363fe60376fb7ab1bf0e6052b0fcae62cfd
SUDPro <stepan.kurin@yandex.ru>
2019-11-29 12:21:39 +0300

    initial commit

---
+++ src/main/java/ru/itis/WebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.itis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketDemoApplication.class, args);
+	}
+}
